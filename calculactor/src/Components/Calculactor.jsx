8924a2f61f69ed96ba6f953ec0889a323d61c5d9
Vivek Ranjan <infovivek.ranjan@gmail.com>
2021-09-17 21:36:45 +0530

    Most of the functionality done in calculator

--- calculactor/src/Components/Calculactor.jsx
+++ calculactor/src/Components/Calculactor.jsx
@@ -8,6 +8,22 @@ function Calculactor() {
     setQuery(e.target.value);
   };
 
+  const handleClick = (e) => {
+    setQuery(query.concat(e.target.name));
+  };
+
+  const handleCalculate = () => {
+    setQuery(eval(query));
+  };
+
+  const handleClear = () => {
+    setQuery("");
+  };
+
+  const calculatePer = () => {
+    setQuery(eval(query / 100));
+  };
+
   return (
     <div>
       <h1>Calculactor</h1>
@@ -15,46 +31,80 @@ function Calculactor() {
       <section>
         <div className={styles.calculactorCont}>
           <div>
-            <input
-              placeholder="Calculate"
-              onChange={handleChange}
-              value={query}
-            />
+            <input onChange={handleChange} value={query} placeholder="00" />
           </div>
 
           <div>
-            <button>C</button>
-            <button>E</button>
-            <button>%</button>
-            <button>=</button>
+            <button onClick={handleClear} style={{ color: "#EF7624" }}>
+              C
+            </button>
+            <button style={{ color: "#EF7624" }}>E</button>
+            <button onClick={calculatePer} style={{ color: "#EF7624" }}>
+              %
+            </button>
+            <button onClick={handleCalculate} className={styles.equalBtn}>
+              =
+            </button>
           </div>
 
           <div>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>/</button>
+            <button onClick={handleClick} name="7">
+              7
+            </button>
+            <button onClick={handleClick} name="8">
+              8
+            </button>
+            <button onClick={handleClick} name="9">
+              9
+            </button>
+            <button onClick={handleClick} name="/" style={{ color: "#EF7624" }}>
+              /
+            </button>
           </div>
 
           <div>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>*</button>
+            <button onClick={handleClick} name="4">
+              4
+            </button>
+            <button onClick={handleClick} name="5">
+              5
+            </button>
+            <button onClick={handleClick} name="6">
+              6
+            </button>
+            <button onClick={handleClick} name="*" style={{ color: "#EF7624" }}>
+              *
+            </button>
           </div>
 
           <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>-</button>
+            <button onClick={handleClick} name="1">
+              1
+            </button>
+            <button onClick={handleClick} name="2">
+              2
+            </button>
+            <button onClick={handleClick} name="3">
+              3
+            </button>
+            <button onClick={handleClick} name="-" style={{ color: "#EF7624" }}>
+              -
+            </button>
           </div>
 
           <div>
-            <button>00</button>
-            <button>0</button>
-            <button>.</button>
-            <button>+</button>
+            <button onClick={handleClick} name="00">
+              00
+            </button>
+            <button onClick={handleClick} name="0">
+              0
+            </button>
+            <button onClick={handleClick} name=".">
+              .
+            </button>
+            <button onClick={handleClick} name="+" style={{ color: "#EF7624" }}>
+              +
+            </button>
           </div>
         </div>
       </section>
